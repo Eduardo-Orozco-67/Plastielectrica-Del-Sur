3658379e793172cbffbaef05d4740917dea8f060
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2021-04-10 00:43:02 -0500

    primera edicion

---
+++ src/plastielectrica/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package plastielectrica;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Este es nuestro proyecto");
+        System.out.println("DE UNA FERRETERIA");
+        // TODO code application logic here
+    }
+}
