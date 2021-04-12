315f4c0b2431ab105716c10b9af2190ec85b534b
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2021-04-11 23:26:46 -0500

    Creacion de metodos get y set en empleado y persona

--- src/plastielectrica/Empleado.java
+++ src/plastielectrica/Empleado.java
@@ -1,15 +1,46 @@
 
 package plastielectrica;
 
+import java.util.Scanner;
+
 public class Empleado extends Persona{
     
-    
+     int ID;
+     String RFC;
+     String aguinaldo;
+     
     Empleado()
     {
         super();
-        String RFC;
-        String aguinaldo;
-        int id;
-                
+        RFC="NULL";
+        aguinaldo="NULL";
+        ID=0;      
     }
+    
+    public void set_RFC(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void set_aguinaldo(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setid(int ID) {
+        this.ID= ID;
+    }
+
+
+
+    public String get_RFC() {
+        return direccion;
+    }
+
+    public String get_aguinaldo() {
+        return telefono;
+    }
+
+    public int get_id() {
+        return edad;
+    }
+
 }
