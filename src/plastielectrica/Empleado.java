5c77e9fbd4476262794e36959b08b65abae16f17
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2021-04-11 23:16:41 -0500

    creacion constrcutor empleado

--- src/plastielectrica/Empleado.java
+++ src/plastielectrica/Empleado.java
@@ -4,4 +4,12 @@ package plastielectrica;
 public class Empleado extends Persona{
     
     
+    Empleado()
+    {
+        super();
+        String RFC;
+        String aguinaldo;
+        int id;
+                
+    }
 }
