81a1af5fb9b73a61b5c206c71ba6c652b4df2865
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2021-04-12 00:28:40 -0500

    creacion sets

--- src/plastielectrica/Herramientas.java
+++ src/plastielectrica/Herramientas.java
@@ -10,10 +10,31 @@ public class Herramientas {
     int Precio;
     int cantidad;
     int Num_Piezas;   
- 
+
+    
+    public void set_Articulo(String Articulo)
+    {
+       this.Articulo=Articulo;
+    }
+    
      public void set_garantia(String garantia)
     {
         this.garantia=garantia;
     }
      
+    public void set_precio(int Precio)
+    {
+        this.Precio=Precio;
+    }
+    
+        public void set_Marca(String Marca)
+    {
+        this.Marca=Marca;
+    }
+        
+        public void set_Precio(int Precio)
+        {
+            this.Precio=Precio;
+        }
+    
 }
