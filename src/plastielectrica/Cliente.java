0f88c697c9c200f6da9098510d48bd6c3e848ed9
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2021-04-11 23:06:27 -0500

    3ra Edicion

--- src/plastielectrica/Cliente.java
+++ src/plastielectrica/Cliente.java
@@ -3,4 +3,12 @@ package plastielectrica;
 
 public class Cliente extends Persona {
     
+    Cliente()
+    {
+        super();
+        String RFC;
+        String TipoCliente;
+        int NumCliente;
+                
+    }
 }
