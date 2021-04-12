5a7b9e03ec65699156d625f7c227f558b5c16133
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2021-04-11 23:34:34 -0500

    Creacion de metodos get y set en empleado y cliente

--- src/plastielectrica/Cliente.java
+++ src/plastielectrica/Cliente.java
@@ -2,13 +2,40 @@
 package plastielectrica;
 
 public class Cliente extends Persona {
-    
+    String RFC;
+    String TipoCliente;
+    int NumCliente;
     Cliente()
     {
         super();
-        String RFC;
-        String TipoCliente;
-        int NumCliente;
+        RFC="NULL";
+        TipoCliente="NULL";
+        NumCliente=0;
                 
+    }
+    
+     public void set_RFC(String RFC) {
+        this.RFC= RFC;
+    }
+
+    public void set_TipoCliente(String TipoCliente) {
+        this.TipoCliente = TipoCliente;
+    }
+
+    public void set_NumCliente(int NumCliente) {
+        this.NumCliente= NumCliente;
+    }
+
+
+    public String get_RFC() {
+        return RFC;
+    }
+
+    public String get_TipoCliente() {
+        return TipoCliente;
+    }
+
+    public int get_NumCliente() {
+        return NumCliente;
     }
 }
