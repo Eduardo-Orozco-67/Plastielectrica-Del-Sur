639fa9a443d6da9bf2514caa61937196716c6e48
JULIO <noe777guillenwigetta@gmail.com>
2021-04-22 13:44:04 -0500

    agregué metodos

--- src/plastielectrica/Empleado.java
+++ src/plastielectrica/Empleado.java
@@ -23,20 +23,7 @@ public class Empleado extends Persona{
             Cuotas = 0 ; 
         }
     
-    //set y get 
-    public void set_RFC(String RFC) {
-        this.RFC = RFC;
-    }
-
-    public void set_aguinaldo(float aguinaldo) {
-        this.aguinaldo = aguinaldo;
-    }
-
-    public void set_id(int ID) {
-        this.ID= ID;
-    }
-
-
+    //GETTERS
     public String get_RFC() {
         return RFC;
     }
@@ -53,65 +40,76 @@ public class Empleado extends Persona{
         return faltas;
     }
 
-    public void setFaltas(int faltas) {
-        this.faltas = faltas;
-    }
-
     public int getRetardos() {
         return retardos;
-    }
-
-    public void setRetardos(int retardos) {
-        this.retardos = retardos;
-    }
-
-    public int getHT() {
-        return HT;
-    }
-
-    public void setHT(int HT) {
-        this.HT = HT;
     }
 
     public int getSS() {
         return SS;
     }
 
-    public void setSS(int SS) {
-        this.SS = SS;
-    }
-
     public int getAnti() {
         return anti;
-    }
-
-    public void setAnti(int anti) {
-        this.anti = anti;
     }
 
     public int getHE() {
         return HE;
     }
 
-    public void setHE(int HE) {
-        this.HE = HE;
-    }
-
     public int getCuotas() {
         return Cuotas;
     }
 
+    public float getSueldo() {
+        return sueldo;
+    }
+    
+    //SETTERS
     public void setCuotas(int Cuotas) {
         this.Cuotas = Cuotas;
     }
 
-
-    public float getSueldo() {
-        return sueldo;
-    }
-
     public void setSueldo(float sueldo) {
         this.sueldo = sueldo;
+    }
+    
+    public void setHE(int HE) {
+        this.HE = HE;
+    }
+    
+    public void setAnti(int anti) {
+        this.anti = anti;
+    }
+    
+    public void setSS(int SS) {
+        this.SS = SS;
+    }
+    
+    public int getHT() {
+        return HT;
+    }
+
+    public void setFaltas(int faltas) {
+        this.faltas = faltas;
+    }
+    public void set_RFC(String RFC) {
+        this.RFC = RFC;
+    }
+
+    public void set_aguinaldo(float aguinaldo) {
+        this.aguinaldo = aguinaldo;
+    }
+
+    public void set_id(int ID) {
+        this.ID= ID;
+    }
+    
+    public void setRetardos(int retardos) {
+        this.retardos = retardos;
+    }
+
+    public void setHT(int HT) {
+        this.HT = HT;
     }
     
      @Override
@@ -121,55 +119,37 @@ public class Empleado extends Persona{
         System.out.print("Dame el ID ");
         set_id(x.nextInt());
         System.out.print("Dame el RFC ");
-        set_RFC(x.next());
-//        System.out.print("Dame el tipo de aguinaldo");
-//        set_aguinaldo(x.next());
     }
-    
-    //metodo para pedir datos del empleado quincenal
+
+    //metodos de llenado de datos
     public void empQuincenal(){
         int cantEmp;
         Empleado emp = new Empleado();
         System.out.println("\n");
-        System.out.println("Ingresar la cantidad de empleados, los cuales se encuentran activos actualmente: \n");
+        System.out.println("EMPLEADO QUINCENAL");
+        System.out.println("Ingresar la cantidad de empleados, los cuales se encuentran activos actualmente: ");
         cantEmp = x.nextInt();
         for (int i = 0; i < cantEmp; i++) {
             emp.llenado();
-            System.out.println("Ingresar el sueldo del empleado: \n");
+            System.out.println("Ingresar el sueldo del empleado: ");
             setSueldo(x.nextFloat());
-            System.out.print("Ingresar las faltas acumuladas del empleado : \n");
+            System.out.print("Ingresar las faltas acumuladas del empleado : ");
             setFaltas(x.nextInt());
-            System.out.println("Ingresar los retardos del empleado: \n");
+            System.out.println("Ingresar los retardos del empleado: ");
             setRetardos(x.nextInt());
-            System.out.println("Ingresar el total de HT: \n");
+            System.out.println("Ingresar el total de HT: ");
             setHT(x.nextInt());
-            System.out.println("Ingresar el porcentaje el cual se le hace al empleado para pagar el seguro social: \n");
+            System.out.println("Ingresar el porcentaje el cual se le hace al empleado para pagar el seguro social: ");
             setSS(x.nextInt());
             System.out.println("\n");
             
         }
     }
     
-    //metodo para calcular el aguinaldo quincenal
-    public void aguinaldoQuin(){
-        int Meses;
-        float cash, res;
-        System.out.println("\n");
-        System.out.println("EMPLEADO QUINCENAL");
-        System.out.println("Ingresar las quincenas que lleva trabajando el empleado en la empresa: \n");
-        Meses = x.nextInt();
-        System.out.println("Ingresar el sueldo que recibe el empleado: ");
-        cash=x.nextFloat();
-        cash = cash/15;
-        res = cash*Meses;
-        System.out.println("El aguinaldo a recibir es de: " + res + " pesos.");
-        System.out.println("\n");
-    }
-    
-    //metodo para pedir datos del empleado mensual
     public void empMensual(){
         int cantEmp;
         Empleado emp = new Empleado();
+        System.out.println("EMPLEADO MENSUAL");
         System.out.println("\n");
         System.out.println("Ingresar la cantidad de empleados, los cuales se encuentran activos actualmente: \n");
         cantEmp = x.nextInt();
@@ -195,7 +175,39 @@ public class Empleado extends Persona{
         }
     }
     
-   //metodo para calcular el aguinaldo mensual
+    //metodos para mostar los datos guardados anteriormente
+    public void mostEmpQuincenal(){
+        super.mostrar();
+        System.out.println("Sueldo: " + getSueldo() + "\n"
+                + "Faltas: " + getFaltas() + "\n"
+                + "Retardos: " + getRetardos() + "\n"
+                + "HT: " + getHT() + "\n"
+                + "Porcentaje a descontar por el SS: " + getSS());
+    }
+    
+    
+    public void mostEmpMensual(){
+        super.mostrar();
+        System.out.println("Sueldo: " + getSueldo() + "\n" 
+                + "Faltas: " + getFaltas() + "\n" 
+                + "Retardos: " + getRetardos() + "\n"
+                + "HT: " + getHT() + "\n"
+                + "Porcentaje a descontar por el SS: " + getSS() + "\n" 
+                + "Años trabajados: " + getAnti() + "\n"
+               + "HE: " + getHE() + "\n"
+                + "Descuento por cuotas: " + getCuotas());
+    }
+    
+    //metodos para calcular los respectivos pagos de los empleados
+    public void calpagoQ(){
+        
+    }
+    
+    public void calpagoM(){
+        
+    }
+    
+    //metodos para calcular los aguinaldos
     public void aguinaldoMen(){
         int Meses;
         float cash, res;
@@ -212,11 +224,20 @@ public class Empleado extends Persona{
         System.out.println("\n");
     }
     
-    public void mostEmpQuincenal(){
-        
-    }
-    public void mostEmpMensual(){
-        
+        //metodo para calcular el aguinaldo quincenal
+    public void aguinaldoQuin(){
+        int Meses;
+        float cash, res;
+        System.out.println("\n");
+        System.out.println("EMPLEADO QUINCENAL");
+        System.out.println("Ingresar las quincenas que lleva trabajando el empleado en la empresa: \n");
+        Meses = x.nextInt();
+        System.out.println("Ingresar el sueldo que recibe el empleado: ");
+        cash=x.nextFloat();
+        cash = cash/15;
+        res = cash*Meses;
+        System.out.println("El aguinaldo a recibir es de: " + res + " pesos.");
+        System.out.println("\n");
     }
     
 }//class
