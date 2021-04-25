34706f4abb345e8b60a3a21c74d4d5359a9d242b
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2021-04-24 19:10:11 -0500

    Edicion 24 de abril menu principal

--- src/plastielectrica/Principal.java
+++ src/plastielectrica/Principal.java
@@ -6,10 +6,13 @@ import java.util.Scanner;
 public class Principal {
 
     public static void main(String[] args) {
-      String respuesta;int op,i;
+      String respuesta;
+      int op,i, res, r1, r2,r3,r4,r5;
         Scanner x = new Scanner(System.in);
           //creamos uestra arraylista para los articulos
-       ArrayList<Herramientas> OBH = new ArrayList<>();
+          ArrayList<Herramientas> OBH = new ArrayList<>();
+       
+        //INFORMACION DE BIENVENIDA 
         System.out.println("*************************************");
         System.out.println("*****\tPlastielectrica Del Sur\t*****");
         System.out.println("*************************************");
@@ -19,6 +22,74 @@ public class Principal {
         System.out.println("Numero telefonico de la empresa: 9624587596\n");
         System.out.println("Horario de servicio: 8:00 AM - 4:00 PM, de Lunes a Sabado!");
 
+        //menu principal 
+          System.out.println("\n-----MENU-----");
+          System.out.println("\n-----Elige una opcion-----");
+          System.out.println(" 1.- Clientes");
+          System.out.println(" 2.- Empleados");
+          System.out.println(" 3.- Articulos");
+          System.out.println(" 4.- Notas");
+          System.out.println(" 5.- Estadisticas");
+          System.out.println(" 6.- Salir");
+          System.out.println();
+          res = x.nextInt();
+          System.out.println();
+
+          //switch menu pricipal
+             switch (res) 
+             { 
+                case 1:
+                     //submenu de cliente
+                     System.out.println("\n-----Que deseas hacer?-----");
+                     System.out.println(" 1.- Anadir Cliente");
+                     System.out.println(" 2.- Editar Cliente");
+                     System.out.println(" 3.- Eliminar Cliente");
+                     r1=x.nextInt();
+                        switch(r1)
+                        {
+                            
+                        }
+                break;
+                 
+                case 2:
+                    //submenu de Empleado 
+                     System.out.println("\n-----Que deseas hacer?-----");
+                     System.out.println(" 1.- Anadir Empleado");
+                     System.out.println(" 2.- Editar Empleado");
+                     System.out.println(" 3.- Eliminar Empleado");
+                     r2=x.nextInt();
+                        switch(r2)
+                        {
+                            
+                        }
+                break;     
+                
+                case 3:
+                    //submenu de Empleado 
+                     System.out.println("\n-----Que deseas hacer?-----");
+                     System.out.println(" 1.- Anadir Articulo");
+                     System.out.println(" 2.- Editar Articulo");
+                     System.out.println(" 3.- Eliminar Articulo");
+                     r3=x.nextInt();
+                        switch(r3)
+                        {
+                            
+                        }
+                break;
+                
+                case 4:
+                    //submenu de Empleado 
+                     System.out.println("\n-----Que deseas hacer?-----");
+                     System.out.println(" 1.- Anadir Empleado");
+                     System.out.println(" 2.- Editar Empleado");
+                     System.out.println(" 3.- Eliminar Empleado");
+                     r4=x.nextInt();
+                        switch(r4)
+                        {
+                            
+                        }
+                break;      
+             }
         
         do{
             System.out.println("\tMenú\t");
@@ -69,7 +140,7 @@ public class Principal {
                     break;
             }
         }while(op!=2);
-       
+        
 }
 }
        
