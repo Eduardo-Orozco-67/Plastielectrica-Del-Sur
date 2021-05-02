f0c56e4501125db7d183c3ecc0bd6ebff249c14a
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2021-05-02 11:38:09 -0500

    Nota

--- src/plastielectrica/Nota.java
+++ src/plastielectrica/Nota.java
@@ -6,43 +6,27 @@ public class Nota extends Persona {
     
     double subtotal, total;
     String fecha;
-    String TipoCliente;
-    String NumCliente;
+   
     Nota()
     {
-        super();
-        fecha=" ";
-        TipoCliente=" ";
-        NumCliente=" ";
+
                 
     }
     
     //metodos set
-     public void setRFC(String fecha) {
+     public void setfecha(String fecha) 
+    {
         this.fecha= fecha;
     }
 
-    public void setTipoCliente(String TipoCliente) {
-        this.TipoCliente = TipoCliente;
-    }
-
-    public void setNumCliente(String NumCliente) {
-        this.NumCliente= NumCliente;
-    }
 
 
-    public String getfecha() {
+    public String getfecha() 
+    {
         return fecha;
     }
 
-    public String getTipoCliente() {
-        return TipoCliente;
-    }
 
-    public String getNumCliente() {
-        return NumCliente;
-    }
-    
 
  }      
 
