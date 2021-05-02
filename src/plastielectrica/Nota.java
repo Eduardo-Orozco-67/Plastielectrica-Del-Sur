b2f54706d3b62ef038095ab5e2fed4e2546c285f
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2021-05-02 13:17:13 -0500

    edicio menu notas

--- src/plastielectrica/Nota.java
+++ src/plastielectrica/Nota.java
@@ -2,15 +2,14 @@
 package plastielectrica;
 
 
-public class Nota extends Persona {
+public class Nota extends Herramientas {
     
     double subtotal, total;
     String fecha;
    
     Nota()
-    {
-
-                
+    {      
+        super();   
     }
     
     //metodos set
@@ -26,6 +25,87 @@ public class Nota extends Persona {
         return fecha;
     }
 
+    
+    
+    @Override
+    public void setArticulo(String articulo) {
+        this.articulo = articulo;
+    }
+
+    @Override
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public void setUnidades(String unidades) {
+        this.unidades = unidades;
+    }
+
+    @Override
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public void setGarantia(int garantia) {
+        this.garantia = garantia;
+    }
+
+    @Override
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String getArticulo() {
+        return articulo;
+    }
+
+    @Override
+    public String getTipo() {
+        return tipo;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String getMarca() {
+        return marca;
+    }
+
+    @Override
+    public String getUnidades() {
+        return unidades;
+    }
+
+    @Override
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    @Override
+    public int getGarantia() {
+        return garantia;
+    }
+
+    @Override
+    public float getPrecio() {
+        return precio;
+    }
 
 
  }      
